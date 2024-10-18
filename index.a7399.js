b73ed2c0b2614ca5628636e9c38dd5ef1d3d7e3d
Boosh-2023 <aaron@harborfi.io>
2024-10-18 22:22:35 +0800

    3.8.4 version16

--- index.a7399.js
+++ index.a7399.js
@@ -1,4 +1,4 @@
-System.register(["./application.1c0a9.js"], function (_export, _context) {
+System.register(["./application.e1bae.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
