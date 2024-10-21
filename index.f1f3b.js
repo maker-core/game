459855a9d1087b62e50c5aadd16d54639ba68bd5
Boosh-2023 <aaron@harborfi.io>
2024-10-21 16:26:10 +0800

    ad3

--- index.f1f3b.js
+++ index.f1f3b.js
@@ -1,4 +1,4 @@
-System.register(["./application.885d7.js"], function (_export, _context) {
+System.register(["./application.ddbc3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
