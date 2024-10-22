84b6907fc34e65da921f8dba2c3319df9945bc28
Boosh-2023 <aaron@harborfi.io>
2024-10-22 18:05:05 +0800

    ad2

--- application.0f23f.js
+++ application.0f23f.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.ee59f.json';
+          this.settingsPath = 'src/settings.027f5.json';
           this.showFPS = false;
         }
         init(engine) {
--- index.456a8.js
+++ index.456a8.js
@@ -1,4 +1,4 @@
-System.register(["./application.d0201.js"], function (_export, _context) {
+System.register(["./application.0f23f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
